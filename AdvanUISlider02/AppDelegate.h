e684cd6742e2ae10e8b841aa39db1e0a6b4dda0d
mac <mac@Thinhdts-MacBook-Pro.local>
2015-07-06 13:51:19 +0700

    Initial Commit

---
+++ AdvanUISlider02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdvanUISlider02
+//
+//  Created by mac on 7/6/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdvanUISlider02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdvanUISlider02
+//
+//  Created by mac on 7/6/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
